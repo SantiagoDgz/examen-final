0292f510a8f265e6f0fb8fee82360fc7936bb3b9
Santiago Dominguez <al45846@un.edu.mx>
2025-12-02 10:35:08 -0600

    ultimo

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var cambiosx = document.getElementById("cambio-sx");
 
+
 function cambio(){
 
      if(cambiosx.checked){
-        document.getElementById("nombre").innerHTML = "Kerio";}
+        document.getElementById("nombre").innerHTML = "Kerio";} 
+        else {
+            document.getElementById("nombre").innerHTML = "Ryu Min -seok";
+        }
+
 }
